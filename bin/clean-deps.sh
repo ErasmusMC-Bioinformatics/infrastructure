02ad2da4cc37ecc289a372b9e850b4d21ab7e030
Helena Rasche <hxr@hx42.org>
2021-03-01 12:14:14 +0100

    safer script

--- bin/clean-deps.sh
+++ bin/clean-deps.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
+set -e -o pipefail
 
-for repo__version in $(ansible-galaxy role list -p roles | awk '{gsub(", ", "\t"); print $2"__"$3}'); do
-	vers="$(echo "$repo__version" | sed 's/.*__//g')"
-	repo="$(echo "$repo__version" | sed 's/__.*//g')"
+if [[ ! -d roles/ ]]; then
+	# No roles dir
+	exit 0
+fi
 
-	# Ignore these, not proper ones.
-	if [[ "$vers" != "(unkonwn" ]]; then
-		results="$(grep "$repo" -A2 requirements.yml)"
-		ec=$?
+for roledir in $(find roles -maxdepth 1 -mindepth 1 -type d); do
+	repo=$(echo $roledir | cut -d/ -f2)
+	vers=$(cat roles/$repo/meta/.galaxy_install_info | grep version | cut -d' ' -f2)
+	echo "Checking $repo / $vers"
 
-		# Not under git's control
-		if (( ec == 0 )); then
-			expected_version=$(echo "$results" | grep version | sed 's/.*version: //g')
+	results="$(grep "$repo" -A2 requirements.yml)"
+	ec=$?
 
-			if [[ "$expected_version" != "master" ]]; then
-				if [[ "$vers" != "$expected_version" ]]; then
-					echo "Removing $repo: $vers != $expected_version";
-					ansible-galaxy remove "$repo"
-				fi
-			fi
+	# Not under git's control
+	if (( ec == 0 )); then
+		expected_version=$(echo "$results" | grep version | sed 's/.*version: //g')
+
+		if [[ "$vers" != "$expected_version" ]]; then
+			echo "Removing $repodir: $vers != $expected_version";
+			ansible-galaxy role remove -p roles $repo
 		fi
 	fi
 done
