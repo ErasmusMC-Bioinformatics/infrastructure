337cb62e10cdc90519b0bfd3bd94a5f4645b7a91
Helena Rasche <hexylena@galaxians.org>
2022-12-05 16:01:44 +0100

    Add edgeR tool

---
+++ files/emc-tools/edgeR.R
@@ -0,0 +1,16 @@
+library(edgeR)
+bcv <- 0.2
+
+files = c("control.txt", "treat.txt")
+RG <- readDGE(files)
+bcv <- 0.2
+
+y <- DGEList(counts=RG, group=1:2)
+et <- exactTest(y, dispersion=bcv^2)
+#estimateDisp(y, robust=TRUE)
+
+# Remove pvalue column
+et$table = subset(et$table, select = -PValue)
+# Add deg colum with true/false
+et$table$deg = abs(et$table$logFC) >= 2
+write.table(et$table, file="edger.tsv", quote=FALSE, sep="\t")
