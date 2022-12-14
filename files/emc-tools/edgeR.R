16623e922250c454665d24fa20a14a01484cc677
Helena Rasche <hexylena@galaxians.org>
2022-12-14 17:04:33 +0100

    EdgeR improvements

--- files/emc-tools/edgeR.R
+++ files/emc-tools/edgeR.R
@@ -1,17 +1,37 @@
 library(edgeR)
 bcv <- 0.2
 
-files = c("control.txt", "treat.txt")
-RG <- readDGE(files)
-bcv <- 0.2
+args <- commandArgs(trailingOnly = TRUE)
+should_filter <- args[1] == "TRUE"
 
-y <- DGEList(counts=RG, group=1:2)
+files = dir(pattern='*.txt')
+split_ctrl_trt <- sapply(strsplit(files, "-"), function(x) x[1])
+
+dge <- readDGE(files, group=factor(split_ctrl_trt))
+print(head(dge))
+
+keep <- filterByExpr(y = dge)
+dge <- dge[keep, , keep.lib.sizes=FALSE]
+dge <- calcNormFactors(object = dge)
+print(head(dge))
+
+# Does not work for us!
+# dge <- estimateDisp(y = dge)
+
+bcv <- 0.2
+y <- DGEList(counts=dge, group=factor(split_ctrl_trt))
 et <- exactTest(y, dispersion=bcv^2)
+print(head(et))
+
+summary(decideTests(object = et, lfc = 1))
 #estimateDisp(y, robust=TRUE)
 
 # Remove pvalue column
 et$table = subset(et$table, select = -PValue)
 # Add deg colum with true/false
 et$table$deg = abs(et$table$logFC) >= 2
+if (should_filter) {
+	et$table = et$table[et$table$deg,]
+}
 #write.table(et$table, file="edger.tsv", quote=FALSE, sep="\t")
 write.table(data.frame("Gene"=rownames(et$table),et$table),"edger.tsv", row.names=FALSE, sep="\t", quote=FALSE)
